5b6eed578573b9b7ffb5a6866a068af8b180c4bf
beratkaya49 <114297047+beratkaya49@users.noreply.github.com>
2024-10-19 12:17:06 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const { AoiClient } = require("aoi.js");
+
+const client = new AoiClient({
+    token: process.env.token, // Here goes the Token you copied earlier!
+    prefix: "!!", // Here goes the prefix you want to use for your bot!
+    intents: ["MessageContent", "Guilds", "GuildMessages", "GuildPresences", "GuildVoiceStates"],
+    events: ["onMessage", "onInteractionCreate"],
+    database: {
+        type: "aoi.db",
+        db: require("@aoijs/aoi.db"),
+        tables: ["main"],
+        path: "./database/",
+        extraOptions: {
+            dbType: "KeyValue"
+        }
+        
+    }
+});
+
+client.loadCommands(`./komutlar/`)
+
+require('./degisken/degiskenler.js')(client);
